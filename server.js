9dca7a505d4b92960c2fb4be484dc34295e5df6f
ntxareed <ntxareed@outlook.jp>
2026-04-17 23:19:17 +0900

    Serve index.html on root routea

--- server.js
+++ server.js
@@ -53,3 +53,7 @@ app.get("/posts", (req, res) => {
 // ===== 起動 =====
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
